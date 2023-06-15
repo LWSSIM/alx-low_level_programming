70134771a63ded33ea2ba0039963aa2f2eb8043f
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-15 10:38:40 -0700

    print string(printf)+retur0+betty

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+/* This program prints const string using printf */
+
+#include <stdio.h>
+
+/**
+ * main - print string using printf
+ * Return: 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
