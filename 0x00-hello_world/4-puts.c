ba5d62e2fcb169fd14cbd6d66956a4784bfb09c5
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-15 10:14:20 -0700

    Print const string +\n

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 /*This C program prints a string*/
 
+/**
+ * prints a const string + \n
+ */
 #include <stdio.h>
 
-int main(void){
-	puts("\"Programing is like building a multilingual puzzle");
+int main(void)
+{
+	puts("\"Programing is like building a multilingual puzzle\n");
 	return 0;
 }
