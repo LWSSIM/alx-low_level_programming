7cf3da8813a33e609dad89adb18184c9e2205228
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-15 10:19:24 -0700

    klk

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
 /*This C program prints a string*/
 
+#include <stdio.h>
+
 /**
  * prints a const string + \n
  */
-#include <stdio.h>
-
 int main(void)
 {
-	puts("\"Programing is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programing is like building a multilingual puzzle");
+	(return 0);
 }
