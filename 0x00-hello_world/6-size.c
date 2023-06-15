da198fbe0001aa9acf71f7929516154a3ea5237a
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-15 11:36:08 -0700

    Print size of types using sizeof + betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
-/* This program prints the size of various types on the computer its is ran on */
+/* This program prints the size of various types on the computer */
 
 #include <stdio.h>
 
 /**
- * main - print types and their sizes 
+ * main - print types and their sizes
  * Return: 0
  */
 int main(void)
@@ -11,7 +11,7 @@ int main(void)
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(S)\n", sizeof(int));
 	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
-	printf("Size of a long long: %lu bytes(s)\n", sizeof(long long));
+	printf("Size of a long long int: %lu bytes(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
 	return (0);
