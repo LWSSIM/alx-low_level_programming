8f49e84be44b1ee9253d86117f45adb15c706464
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-15 11:30:00 -0700

    print size of types in a computer ( sizeof(types))

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,7 +10,7 @@ int main(void)
 {
 	printf("Size of a char: %lu byte(s)\n", sizeof(char));
 	printf("Size of an int: %lu byte(S)\n", sizeof(int));
-	printf("Size of a long: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
 	printf("Size of a long long: %lu bytes(s)\n", sizeof(long long));
 	printf("Size of a float: %lu byte(s)\n", sizeof(float));
 
