d5f1e4d64b668655f26b50a3d1d8269f9e5c03e3
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-16 16:52:15 -0700

    for loop ascii {a..z}

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+/* This program use ASCII to putchar {a..z}*/
+
+#include <stdio.h>
+
+/**
+ * main - print letters {a..z} +\n
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar((char)i);
+	}
+	putchar("\n");
+
+	return 0;
+}
