a75c85e6e7e39608b97ca9b332534d7f7ffe36c0
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-15 17:15:37 -0700

    Program to print to stderr (write function)+betty1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,9 +9,9 @@
  */
 int main(void)
 {
-	char quote[]="and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
-	write(2, quote, sizeof(quote) - 1);
+	write(2, q, sizeof(q) - 1);
 
 	return (1);
 }
