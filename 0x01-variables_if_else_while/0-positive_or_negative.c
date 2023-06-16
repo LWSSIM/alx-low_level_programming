e4a1efe04de1fe2cbaba4d1e254e942f8ca8466c
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-16 12:26:11 -0700

    gen rand int n + print n (-/+/0)

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,28 @@
+/* This program generate rand n then print sign */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - gen rand n > print value(-+)
+ * Return (0)
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	
+	if (n<0){
+		printf("%d is negative\n",n);
+	}else if (n>0){
+		printf("%d is positive\n",n);
+	}else {
+		printf("%d is zero\n",n);
+	}
+	
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,14 @@
+#This directory will touch on Vars,if,else and while(according to C lang)
+
+<h1>Main Objective</h1>
+
+<ol>
+<li>Arethmetic operators</li>
+<li>Logical opps</li>
+<li>Relational opps</li>
+<li>Conditionals[if/else]</li>
+<li>Loops[while/for]</li>
+<li>Variables[assign/print(values)]</li>
+</ol>
+
+<p>Kindly consult The C Programming Lang Handbook</p>
