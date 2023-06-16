20dd461d18906d6be2fd498545db9e6096b5e2fb
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-16 13:10:06 -0700

    print conditional str based on z last digit on rand n

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,33 @@
+/* This prog gen n + print conditional(n) str */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* main - gen rand n + check last digit + print
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	int z = (n % 10);
+	printf("Last digit of %d is %d ", n, z);
+		if (z > 5)
+		{
+			printf("and is greater than 5\n");
+		}
+		else if (z < 6 && z != 0)
+		{
+			pritnf("and is less than 6 and not 0\n");
+		}
+		else if (z = 0)
+		{
+			pritnf("and is 0\n");
+		}
+	return (0);
+}
