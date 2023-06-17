6faf4265a93ef6ad5aa6fce82de52ae2cbb39e41
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-16 17:26:14 -0700

    intdigits base 10

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,6 +10,7 @@
 int main(void)
 {
 	char i;
+
 	for (i = 'a'; i <= 'z'; i++)
 	{
 		if (i != 'q' && i != 'e')
---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+/* print all single digit of base 10 */
+
+#include <stdio.h>
+
+/**
+ * main - print base 10 digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	for (i = 0; i<= 10; i++)
+	{
+		printf('i');
+	}
+	return (0);
+}
