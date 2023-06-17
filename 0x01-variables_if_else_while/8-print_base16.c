1148d8d57715393c45b6859225ee9d5e6e18b5cc
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-16 18:16:21 -0700

    print #16 hex

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -3,19 +3,20 @@
 #include <stdio.h>
 
 /**
- * main - putchar #16 nums with for loop
+ * main - putchar #16 nums with for loop+condition
+ * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 0;i < 16; i++)
+	for (i = 0; i < 16; i++)
 	{
-		char d = (i < 10) ? ('0' + i) : ('a' + i - 10);
-		putchar(i);
+	char d = (i < 10) ? ('0' + i) : ('a' + i - 10);
+		putchar(d);
 	}
 
 	putchar('\n');
-	
+
 	return (0);
 }
