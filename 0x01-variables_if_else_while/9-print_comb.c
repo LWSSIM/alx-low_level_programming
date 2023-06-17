c2fe5817aedfb255de9371a5f52f4e4a2fa6a731
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-16 18:30:21 -0700

    print digits ','

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+/* Print all possible combinations of single digit nums*/
+
+#include <stdio.h>
+
+/**
+ * main - use for loop
+ *Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i <=9; i++)
+	{
+		putchar(i + '0');
+		if (i < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	
+	putchat('\n');
+
+	return (0);
+}
