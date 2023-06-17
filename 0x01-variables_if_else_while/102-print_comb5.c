9de003260e0a3c4e1b32adbcb8529da082db9f1f
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-17 05:25:46 -0700

    prints all possible combinations of two two-digit number

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i <= 98; i++)
 	{
-		for (j = i; j <= 99; j++)
+		for (j = i + 1; j <= 98; j++)
 		{
 
 			putchar(i / 10 + '0');
@@ -22,7 +22,7 @@ int main(void)
 			putchar(j / 10 + '0');
 			putchar(j % 10 + '0');
 
-			if (i < 99 || j < 99)
+			if (i != 98 || j != 99)
 			{
 				putchar(',');
 				putchar(' ');
