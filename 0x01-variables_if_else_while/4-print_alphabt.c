6b5872a8a00450fd946bbb07a5fa0237bb93d599
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-16 17:15:25 -0700

    print alpha with exception

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+/* print alpha != qe*/
+
+#include <stdio.h>
+
+/**
+ * main - loop alphabet with exceptions
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
