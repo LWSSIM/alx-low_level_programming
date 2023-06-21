04f9b8a992d6d1eb32abb9d634f6f0c56b70c188
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-21 11:15:47 -0700

    edited 100-ntimestable to be less than 40 lines

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,63 +1,46 @@
 /* print n times table */
-
 #include "main.h"
-
 /**
  * print_times_table - nested loops to print n * table
- *
  * @n: number to be multiplied
  */
 void print_times_table(int n)
 {
 	int i, j;
 
-	if (n < 0 || n > 15)
+	if (n >= 0 && n <= 15)
 	{
-		return;
-	}
 		for (i = 0; i <= n; i++)
 		{
 			for (j = 0; j <= n; j++)
 			{
 				int c = i * j;
-				if (j != 0)
+
+				if (j == 0)
+					_putchar('0' + c);
+				else if (c < 10 && j != 0)
 				{
-					if (c < 10)
-					{
-						_putchar(' ');
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else if (c < 100)
-					{
-						_putchar(' ');
-						_putchar(' ');
-					}
-					else
-					{
-						_putchar(' ');
-					}
-				}
-				if (c < 10)
-				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
 					_putchar(c + '0');
 				}
-				else if (c < 100)
+				else if (c >= 10 && c < 100)
 				{
-					_putchar((c / 10) + '0');
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(c / 10 + '0');
 					_putchar(c % 10 + '0');
 				}
-				else
+				else if (c >= 100)
 				{
+					_putchar(',');
+					_putchar(' ');
 					_putchar((c / 100) + '0');
 					_putchar((c / 10) % 10 + '0');
 					_putchar (c % 10 + '0');
-				}
-				if (j != n)
-				{
-					_putchar(',');
-				}
-			}
+				}}
 			_putchar('\n');
-		}
-}
+}}}
