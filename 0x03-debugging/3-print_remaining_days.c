f6c83d452491505d4304317fa2529fb323c1e078
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-22 09:14:27 -0700

    version 2 3-debug

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -2,15 +2,16 @@
 #include "main.h"
 
 /**
- * print_remaining_days - how many days are left in the year, accounting for leap years
- * @month: month number
+ * print_remaining_days - takes a date and prints how many days are
+ * left in the year, taking leap years into account
+ * @month: month in number format
  * @day: day of the month
  * @year: year
  * Return: void
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 4 == 0) || (!(year % 100 == 0) && (year % 400 == 0)))
+	if ((year % 4 == 0) || ((year % 100 == 0) && (year % 400 == 0)))
 	{
 		if ( month >= 2 && day >=60)
 		{
