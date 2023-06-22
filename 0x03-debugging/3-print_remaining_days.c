2205f45ec4ffa317a5ef2142a405f73348a54c8e
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-22 12:23:38 -0700

    Added finale version of leap year problem

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -11,7 +11,8 @@
  */
 void print_remaining_days(int month, int day, int year)
 {
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0 && year % 100 != 0))
+	if ((year % 100 == 0 && year % 400 == 0) ||
+		(year % 4 == 0 && year % 100 != 0))
 	{
 		if (month > 2 && day >= 60)
 		{
@@ -21,7 +22,7 @@ void print_remaining_days(int month, int day, int year)
 			printf("Remaining days: %d\n", 366 - day);
 	}
 	else
-	{9. Fizz-Buzz
+	{
 		if (month == 2 && day == 60)
 		{
 		printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
