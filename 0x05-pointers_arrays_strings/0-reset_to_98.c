4018059044ec22f7f9b24b826e03233085841b84
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-26 00:03:37 -0700

    added 0-

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - update input value to 98
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*_putchar*/
+int _putchar(char c);
+
+/*update value of a pointer*/
+void reset_to_98(int *n);
+
+
+#endif
