e346eb27eb8d4e882db4bf234cb66a7c86695b3b
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-26 11:33:25 -0700

    8-new version( edge cases)

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,10 +10,16 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n - 1; i++)
+	if (n <= 0)
 	{
-		printf("%d, ", a[i]);
+		printf("\n");
 	}
-	printf("%d\n", a[n - 1]);
-
+	else
+	{
+		for (i = 0; i < n - 1; i++)
+		{
+			printf("%d, ", a[i]);
+		}
+			printf("%d\n", a[n - 1]);
+	}
 }
