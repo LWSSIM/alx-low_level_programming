ada091463012d8afdb13f6c0f97258cb932f1a17
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-26 11:22:07 -0700

    8-debug8-debug8-debug8-debug8-debug8-debug8-debug8-debug

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,9 +10,11 @@ void print_array(int *a, int n)
 {
 	int i;
 
-	for (i = 0; i < n; i++)
+	for (i = 0; i < n - 1; i++)
 	{
 		printf("%d, ", a[i]);
 	}
+	printf("%d", a[n - 1]);
+
 	_putchar('\n');
 }
