9ca819be7aead45a6c11e58fcd9d6656410259eb
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-27 10:19:02 -0700

    debg

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -8,19 +8,19 @@
  */
 int main(void)
 {
-	int pswd[50];
+	int pswd[100];
 	int i, x;
 	int sum = 0;
 
 	srand(time(NULL));
 
-	for (i = 0; i < 50; i++)
+	for (i = 0; i < 100; i++)
 	{
-		pswd[i] = rand() % 100;
+		pswd[i] = rand() % 78;
 		putchar(pswd[i] + '0');
 		sum += pswd[i] + '0';
 
-		if ((2772 - sum) - '0' < 100)
+		if ((2772 - sum) - '0' < 78)
 		{
 			x = (2772 - sum) - '0';
 			sum += x;
