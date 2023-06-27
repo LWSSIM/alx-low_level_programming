85f42c073a2b61800ea98411b54850adf2e1a6c3
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-27 07:52:41 -0700

    Now covers overflow, + better sign handeling

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,26 +10,23 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int result = 0;
+	int INT_MAX = 2147483647;
+	int INT_MIN = (-INT_MAX - 1);
 	int x;
 
-	while (s[i] == '+' || s[i] == '-')
+	while (s[i] && (s[i] < '0' || s[i] > '9'))
 	{
 		if (s[i] == '-')
-			sign *= -1;
+			sign = -sign;
 		i++;
 	}
-
-	while (s[i] == ' ')
-	{
-		i++;
-	}
-
 	while (s[i] >= '0' && s[i] <= '9')
 	{
 		x = s[i] - '0';
+
 		if (result > INT_MAX / 10 ||
 				(result == INT_MAX / 10
-				 && digit > INT_MAX % 10))
+				 && x > INT_MAX % 10))
 		{
 			if (sign == 1)
 				return (INT_MAX);
