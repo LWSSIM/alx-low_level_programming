d880e321d443411524696486ecb07cfae6cd49de
LWSSIM <wassim.elmnajja2@gmail.com>
2023-06-26 18:55:38 -0700

    986

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -10,24 +10,34 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int result = 0;
+	int x;
+
+	while (s[i] == '+' || s[i] == '-')
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		i++;
+	}
 
 	while (s[i] == ' ')
-	{
-		i++;
-	}
-	if (s[i] == '-')
-	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
 	{
 		i++;
 	}
 
 	while (s[i] >= '0' && s[i] <= '9')
 	{
-		result = result * 10 + (s[i] - '0');
+		x = s[i] - '0';
+		if (result > INT_MAX / 10 ||
+				(result == INT_MAX / 10
+				 && digit > INT_MAX % 10))
+		{
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
+		}
+
+		result = result * 10 + x;
 		i++;
 	}
 	return (result * sign);
