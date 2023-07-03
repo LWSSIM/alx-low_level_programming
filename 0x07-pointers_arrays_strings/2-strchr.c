73d59a7f4bbeae0c3c768f7913bf932f766bd11b
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-03 15:50:53 -0700

    3

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -8,11 +8,17 @@
  */
 char *_strchr(char *s, char c)
 {
+	if (s == '\0')
+		return (0);
+
 	while (*s != '\0')
 	{
 		if (*s == c)
 			return (s);
 		s++;
 	}
-		return ('\0');
+	if (*s == c)
+		return (c);
+
+	return (0);
 }
