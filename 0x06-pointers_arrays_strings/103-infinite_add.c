41e2d87489ea64868547a75c10cd68e1119b6af8
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-03 10:28:08 -0700

    103-infinite-add

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,41 +10,47 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int len1 = 0, len2 = 0;
-	int i, j, k;
-	int carry = 0;
+	int i = 0, j = 0, k = 0;
+	int sum = 0, x10 = 0, start = 0, temp = 0;
 
-	while (n1[len1] != '\0')
+	while (n1[i] != 0)
+		i++;
+	while (n2[j] != 0)
+		j++;
+
+	if (i >= size_r - 1 || j >= size_r - 1)
+		return (0);
+
+	i--;
+	j--;
+
+	for (; i >= 0 || j >= 0; i--, j--, k++)
 	{
-		len1++;
-	}
-	while (n2[len2] != '\0')
-	{
-		len2++;
-	}
+		sum = x10;
 
-	i = len1 - 1, j = len2 - 1, k = size_r - 1;
-
-	while (i >= 0 || j >=0 || carry > 0)
-	{
-		int dig1, dig2, sum, dig;
-
-		dig1 = (i >= 0) ? n1[i] - '0' : 0;
-		dig2 = (j >= 0) ? n2[j] - '0' : 0;
-
-		sum = dig1 + dig2 + carry;
-		carry = sum / 10;
-		dig = sum % 10;
-
-		if (k < 0)
-		{
+		if (i >= 0)
+			sum += n1[i] - '0';
+		if (j >= 0)
+			sum += n2[j] - '0';
+		x10 = sum / 10;
+		r[k] = sum % 10 + '0';
+		if (k >= size_r - 1)
 			return (0);
-		}
-
-		r[k] = dig + '0';
-
-		i--, j--, k--;
 	}
-
-	return (&r[k + 1]);
+	if (x10 > 0)
+	{
+		if (k >= size_r - 1)
+			return (0);
+		r[k] = x10 + '0';
+		k++;
+	}
+	r[k] = '\0';
+	k--;
+	for (; start < k; k--, start++)
+	{
+		temp = r[k];
+		r[k] = r[start];
+		r[start] = temp;
+	}
+	return (r);
 }
