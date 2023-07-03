05d0be3f60f5a68e8c07cbb57310ffb78a1112be
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-03 14:42:59 -0700

    moded 0-memset(betty/0

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,7 +4,7 @@
  * _memset - copy a cte over n bytes of memory in s adress
  * @s: pointer to string
  * @b: cte input to be coppied
- * @n number of bytes to fill with cte(b)
+ * @n: number of bytes to fill with cte(b)
  * Return: s pointer
  */
 char *_memset(char *s, char b, unsigned int n)
