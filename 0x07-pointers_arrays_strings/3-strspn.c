c8470b780f0074d16d84d5a63c87a7f35e590777
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-03 16:52:59 -0700

    3-strspn

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,7 +12,7 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	int check;
 
-	for (i = 0 s[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		check = 0;
 		for (j = 0; accept[j] != 0; j++)
