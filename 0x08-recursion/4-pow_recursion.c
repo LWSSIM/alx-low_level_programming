a7540753581766c6f820587d5b9de8b40a11834b
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-05 08:42:42 -0700

    4-pow_recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - calculate x^y
+ * @x: integer
+ * @y: exponent
+ * Return: x^y || -1 if y < 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (x == 1 || y == 0)
+		return (1);
+
+	return (x * _pow_recursion(x, y -1));
+}
