9218755781c25e471ba628957c483c5223568560
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-05 09:29:45 -0700

    4

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -10,8 +10,8 @@ int sqrt_calc(int n, int s, int e);
 int _sqrt_recursion(int n)
 {
 	if (n < 0)
-		return (0);
-	if (n > 1 || n % 2 != 0)
+		return (-1);
+	if (n > 1 && n % 2 != 0)
 		return (-1);
 	if (n == 0 || n == 1)
 		return (n);
