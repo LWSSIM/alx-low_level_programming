049cd1543553c27b2110f83363cbd65349915e05
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-05 07:11:04 -0700

    1-print rev recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - print str in rev
+ * @s: pointer to input str
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_print_rev_recursion(++s);
+		_putchar(*s);
+	}
+	else
+		_putchar('\n');
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -4,6 +4,9 @@
 /*print a str*/
 void _puts_recursion(char *s);
 
+/*print str in reverse*/
+void _print_rev_recursion(char *s);
+
 /*putchar*/
 int _putchar(char c);
 
