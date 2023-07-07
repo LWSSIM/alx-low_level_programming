46260988387982d7c669bbcf074bde1ee5bc0190
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-07 16:41:59 -0700

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - print argc n
+ * @argc: arg count
+ * @argv: arg vector
+ * Return: 0 
+ */
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
