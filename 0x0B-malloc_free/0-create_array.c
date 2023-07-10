f4037f9456a0a2b9ef06f8025cc2f2cf4011fb5d
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-10 09:59:47 -0700

    0-create-array.c

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * create_array - of chars, + init with specific char
+ * @size: of aray
+ * @c: init char
+ * Return: pointer to arrays 1st address || NULL if size=0/fail
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *p;
+
+	if (size == 0)
+		return (0);
+
+	p = malloc(size * sizeof(char));
+	if (p == '\0')
+		return (0);
+
+	*p = c;
+	return (p);
+}
