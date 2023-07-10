71479bdafc0d6e0788524978e2906cb433361faf
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-10 10:36:01 -0700

    1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * _strdup - duplicate str to new allocated memory space
+ * @str: pointer to input string
+ * Return: pointer to new memory space && NULL if str = null
+ */
+char *_strdup(char *str)
+{
+	char *p;
+	int i, size;
+
+	if (str == 0)
+		return (0);
+
+	size = 0;
+	while (str[size] != '\0')
+		size++;
+
+	p = malloc(sizeof(char) * (size + 1));
+	if (p == 0)
+		return (0);
+	
+	for (i = 0; i < size; i++)
+	{
+		p[i] = str[i];
+	}
+	p[size] = '\0';
+	return (p);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -3,5 +3,10 @@
 
 /*putchar*/
 int _putchar(char c);
+/*create and init char array*/
+char *create_array(unsigned int size, char c);
+/*duplicate str*/
+char *_strdup(char *str);
+
 
 #endif /*main.h*/
