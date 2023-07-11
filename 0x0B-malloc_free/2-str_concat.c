a3d5a757977d5435e76bbbeef5f2eac75b440806
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-10 18:21:05 -0700

    2-strfr

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,14 +12,11 @@ char *str_concat(char *s1, char *s2)
 	char *p;
 	unsigned int z1, z2, i, j;
 
-	if (s1 == 0)
-		s1 = "";
-	else if (s2 == 0)
-		s2 = "";
+	z1 = 0, z2 = 0;
 
-	while (s1[z1] != '\0')
+	while (s1 != 0 && s1[z1] != '\0')
 		z1++;
-	while (s2[z2] != '\0')
+	while (s2 != 0 && s2[z2] != '\0')
 		z2++;
 
 	p = malloc(sizeof(char) * (z1 + z2 + 1));
@@ -27,13 +24,14 @@ char *str_concat(char *s1, char *s2)
 	if (p == 0)
 		return (0);
 
-	for (i = 0; i < z1; i++)
+	for (i = 0; i < z1 && s1 != 0; i++)
 		p[i] = s2[i];
 
-	for (j = 0; j < z2; j++)
+	for (j = 0; j < z2 && s2 != 0; j++)
 		p[z1 + j] = s2[j];
 
 	p[z1 + z2] = '\0';
 
 	return (p);
+	free (p);
 }
