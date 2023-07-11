36e41928b73c202d27ec4e13597e4f36271b6c15
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-11 05:45:18 -0700

    3-alloc_grid

---
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -0,0 +1,38 @@
+#include <stdlib.c>
+#include "main.h"
+
+/**
+ * alloc_grid - return a pointer to a 2 dim int array
+ * @width: row
+ * @height: column
+ * Return: pointer to 2 dim array || NULL
+ */
+int **alloc_grid(int width, int height)
+{
+	int i, j;
+
+	if (width <= 0 || height <= 0)
+		return (0);
+
+	int **grid = (int **)malloc(height * sizeof(int *));
+	if (grid == 0)
+		return (0);
+
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(width * sizeof(int));
+		if (grid[i] == 0)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			return (0);
+		}
+		for (j = 0; j < width; j++)
+		{
+			grid[i][j] = 0;
+		}
+	}
+	return grid;
+}
