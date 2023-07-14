67db924041d8023cee133eaa7aebc7346dc417c5
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-13 17:33:53 -0700

    100

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * _realloc - using malloc / free
+ * @ptr: to old mem block
+ * @old_size: in bytes
+ * @new_size: in bytes
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	unsigned int min_size, i;
+	unsigned char *ptr_byte, *new_ptr_byte;
+	void *new_ptr;
+	
+	if (new_size == old_size)
+		return (ptr);
+
+	if (ptr == 0)
+		return (malloc(new_size));
+	if (new_size == 0 && ptr != 0)
+	{
+		free(ptr);
+		return (0);
+	}
+
+	new_ptr = malloc(new_size);
+	if (new_ptr == 0)
+		return (0);
+
+	min_size = (old_size < new_size) ? old_size : new_size;
+	ptr_byte = (unsigned char *)ptr;
+	new_ptr_byte = (unsigned char *)new_ptr;
+	for (i = 0; i < min_size; i++)
+		new_ptr_byte[i] = ptr_byte[i];
+
+	free(ptr);
+	return (new_ptr);
+}
