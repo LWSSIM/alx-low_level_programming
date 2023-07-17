b0e80bb29e423e62e9a01e22e0a463ebc9a6f35a
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-17 09:28:05 -0700

    added dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - cute doggo info :3
+ * @name: cute doggo name
+ * @age: cute doggo age
+ * @owner: doggo human
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /*DOG_H*/
