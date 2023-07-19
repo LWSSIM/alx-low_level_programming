30ee49e2afc7c08744068acd6425524a84f47ff0
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-19 09:00:15 -0700

    M .h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -9,4 +9,7 @@ void print_name(char *name, void (*f)(char *));
 /*exec a parameter function on each array i*/
 void array_iterator(int *array, size_t size, void (*action)(int));
 
+/*search for an int within array return index*/
+int int_index(int *array, int size, int (*cmp)(int));
+
 #endif /*FUNCTION_POINTERS.H*/
