0b7f9e6f48ef11181c8ce69b29228b9e0f0091bb
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-20 10:00:06 -0700

    1-print_numbers passed as args

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -13,14 +13,11 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	va_list args;
 	unsigned int i;
 
-	if (n == 0)
-		exit(1);
-
 	va_start(args, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(args, int));
+		printf("%i", va_arg(args, int));
 		if (separator && i != n - 1)
 			printf("%s", separator);
 	}
