13456712298adeae95e8dcd9ead76d5b0fe0041f
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-20 09:47:38 -0700

    1-print ints

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+/**
+ * print_numbers - print numbers passed
+ * @separator: str to be printed between occurencens
+ * @n: nb of integers paased
+ */
+void print_numbers(const char *separator, const unsigned n, ...)
+{
+	va_list args;
+	unsigned int i;
+
+	if (n == 0)
+		exit(1);
+
+	va_start(args, n);
+	
+	for (i = 0; i < n; i++)
+	{
+		if (separator && i != n - 1)
+			printf("%d%s", va_arg(args, int), *separator);
+		else
+			printf("%d", va_arg(args, int));
+	}
+	va_end(args);
+	_putchar('\n');
+}
