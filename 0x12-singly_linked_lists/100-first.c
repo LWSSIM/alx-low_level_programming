6c0ed0c4bd3e8e4fcba076e4b50dddf2c4d595b8
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-27 17:26:35 -0700

    *Prototype*__attribute__((construct));

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -7,5 +7,6 @@ void constructor_func(void) __attribute__((constructor));
  */
 void constructor_func(void)
 {
-	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
