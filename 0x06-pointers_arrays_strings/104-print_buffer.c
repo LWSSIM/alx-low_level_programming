7a314555381aa068d846133fccca456a4b92d89a
LWSSIM <wassim.elmnajja2@gmail.com>
2023-07-28 09:35:41 -0700

    last

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -1,40 +1,48 @@
-#include "main.h"
 #include <stdio.h>
 
 /**
- * print_buffer - prints a buffer
- * @b: ptr to mem chunck
- * @size: bytes to be printed from chuck
+ * print_buffer - print the content of a buffer
+ * @b: buffer pointer
+ * @size: size of the buffer
  */
 void print_buffer(char *b, int size)
 {
 	int i, j;
 
 	if (size <= 0)
+	{
 		putchar('\n');
+		return;
+	}
 
 	for (i = 0; i < size; i += 10)
 	{
 		printf("%08x: ", i);
-
-		/*print hex 2chars of buff*/
-		for (j = 0; j < 10; j++)
+		/* Print hexadecimal content (2 chars) of the buffer, 2 bytes at a time */
+		for (j = i; j < i + 10; j++)
 		{
-			if (j <= size - i - 1)
+			if (j < size)
 				printf("%02x", b[j]);
 			else
-				printf("  ");
+				printf("  "); /* Print spaces for empty spaces in the last line */
 			if (j % 2 == 1)
-				putchar(' ');
+				putchar(' '); /* Separate every two bytes with a space */
 		}
 		putchar(' ');
-
-		for (j = 0; j <  10 && i + j < size; j++) /*print chars*/
+		/* Print the content of the buffer as printable characters or '.' */
+		for (j = i; j < i + 10; j++)
 		{
-			if (b[j + i] >= 32 && b[i + j] <= 126)
-				putchar(b[j + i]);
+			if (j < size)
+			{
+				if (b[j] >= 32 && b[j] <= 126) /* Printable characters are in this range */
+					putchar(b[j]);
+				else
+					putchar('.'); /* Non-printable characters are displayed as '.' */
+			}
 			else
-				putchar('.');
+			{
+				putchar(' '); /* Print spaces for empty spaces in the last line */
+			}
 		}
 		putchar('\n');
 	}
