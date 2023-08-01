eae27cb6ffeb08d4d96a5939b6c78e24b82e30bd
LWSSIM <wassim.elmnajja2@gmail.com>
2023-08-01 10:50:12 -0700

    added free(safe)

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,75 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * find_listint_loop - find a loop within a SLL
+ * @head: ptr->head of SLL
+ * Return: node where loop starts || NULL if linear
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *sp, *fp;
+
+	sp = head, fp = head;
+
+	if (head == NULL || head->next == NULL)
+		return (NULL);
+
+	while (fp && fp->next)
+	{
+		sp = sp->next;
+		fp = fp->next->next;
+
+		if (sp == fp)
+		{
+			sp = head;
+			while (sp != fp)
+			{
+				sp = sp->next;
+				fp = fp->next;
+			}
+			return (sp);
+		}
+	}
+	return (NULL);
+}
+
+/**
+ * free_listint_safe - frees a SLLof type listint_t
+ * @h: ptr->ptr->head
+ * Return: size of the SLL that was free'd
+ */
+size_t free_listint_safe(listint_t **h)
+{
+	listint_t *node, *loop_id;
+	size_t node_count;
+	int loop = 1;
+
+	if (!h || !(*h))
+		return (0);
+
+	loop_id = find_listint_loop(*h);
+	
+	for (node_count = 0; (*h != loop_id || loop) && !(*h); *h = node)
+	{
+		node_count++;
+		node = (*h)->next;
+
+		if (*h == loop_id && loop)
+		{
+			if (loop_id == loop_id->next)
+			{
+				free(*h);
+				break;
+			}
+
+			node_count++;
+			node = node->next;
+			free((*h)->next);
+			loop = 0;
+		}
+		free(*h);
+	}
+	*h = NULL;
+	return (node_count);
+}
