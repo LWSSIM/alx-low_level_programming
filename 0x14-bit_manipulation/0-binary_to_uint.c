f66570dd4ded869bdfd85768be1204ae3cfd257e
LWSSIM <127129101+LWSSIM@users.noreply.github.com>
2023-08-04 00:21:55 +0100

    typo 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -18,7 +18,7 @@ unsigned int binary_to_uint(const char *b)
 		if (*b != '0' && *b != '1')
 			return (0);
 
-		x = x << 1; /*left shift to make room foe new bit*/
+		x = x << 1; /*left shift to make room for new bit*/
 
 		if (*b == '1')
 			x = x | 1;/*set least signfcnt bit to 1*/
