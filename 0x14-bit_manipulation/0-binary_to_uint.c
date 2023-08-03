2fdac99a2a6e51cb44ca438c5e8c406cb85ec525
LWSSIM <wassim.elmnajja2@gmail.com>
2023-08-03 10:25:46 -0700

    added 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - convert from binary to U_int
+ * @b: ptr->str(0||1)
+ *
+ * Return: number || 0
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int x;
+	
+	if (!b)
+		return (0);
+	
+	while (*b != '\0')
+	{
+		if (*b != '\0' && *b != '\0')
+			return (0);
+
+		x = x << 1; /*left shift to make room foe new bit*/
+
+		if (*b == '1')
+			x = x | 1;/*set least signfcnt bit to 1*/
+
+		b++;
+	}
+	return (x);
+}
