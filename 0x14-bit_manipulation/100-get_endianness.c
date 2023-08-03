e5dad63221d361d1a6ed2850e293f5a69b886a07
LWSSIM <wassim.elmnajja2@gmail.com>
2023-08-03 12:39:06 -0700

    debug

--- 0x14-bit_manipulation/100-get_endianness.c
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * get_endiannes - check the endianness(multi-data types are stored in mem
+ * get_endianness - check the endianness(multi-data types are stored in mem
  * Return: 1 || 0
  */
-int get_endiannes(void)
+int get_endianness(void)
 {
 	unsigned int num = 1;
 	char *p = (char *)&num;
