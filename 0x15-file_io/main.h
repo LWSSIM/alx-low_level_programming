754ac9f6641872dc756dd06498ac63f21b6243f4
LWSSIM <wassim.elmnajja2@gmail.com>
2023-08-07 11:04:46 -0700

    added 0-read-write

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,8 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stddef.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-
+/*reads a text file & print to stdout*/
+ssize_t read_textfile(const char *filename, size_t letters);
 
 
 #endif /*MAIN_H*/
