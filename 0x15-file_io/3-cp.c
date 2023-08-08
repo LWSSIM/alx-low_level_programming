b03f00e5d460f3f8da8fd154b0fcb6df5452ae9b
LWSSIM <wassim.elmnajja2@gmail.com>
2023-08-07 18:57:40 -0700

    debugged

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,13 +11,15 @@ void print_error(const char *message, int exit_code)
 	exit(exit_code);
 }
 /**
- * open_source_file - open file_from as RDONLY
- * @from: passed file
+ * open_files - open files
+ * @from: passed file_from
+ * @to: passed filr_to
  * Return: filedescriptor
  */
-int open_source_file(const char *from)
+int open_files(const char *from, const char *to)
 {
 	int fildesF = open(from, O_RDONLY);
+	int fildesT;
 
 	if (fildesF == -1)
 	{
@@ -27,17 +29,8 @@ int open_source_file(const char *from)
 			"Error: Can't read from file %s", from);
 		print_error(error_message, 98);
 	}
-	return (fildesF);
-}
-/**
- * open_destination_file - open file_to
- * @to: passed file
- * Return: filedescriptor
- *
- */
-int open_destination_file(const char *to)
-{
-	int fildesT = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
+
+	fildesT = open(to, O_WRONLY | O_CREAT | O_TRUNC, 0664);
 
 	if (fildesT == -1)
 	{
@@ -49,37 +42,33 @@ int open_destination_file(const char *to)
 	}
 	return (fildesT);
 }
-/**
- * read_from_source - reads source file
- * @fildesF: file descriptor
- * @bufr: char buffer
- * Return: content
- */
-ssize_t read_from_source(int fildesF, char *bufr)
-{
-	ssize_t Bread = read(fildesF, bufr, 1024);
 
+/**
+ * copy_data - copy data from filedescriptor to another
+ * @fildesF: fildescriptor of file_from
+ * @fildesT: fildescriptor of file_to
+ */
+void copy_data(int fildesF, int fildesT)
+{
+	ssize_t Bread, Bwriten;
+	char bufr[1024];
+
+	while ((Bread = read(fildesF, bufr, 1024)) > 0)
+	{
+		Bwriten = write(fildesT, bufr, Bread);
+
+		if (Bwriten == -1 || Bread != Bwriten)
+		{
+			close(fildesF);
+			close(fildesT);
+			print_error("Error: Can't write to destination file", 99);
+		}
+	}
 	if (Bread == -1)
 	{
 		close(fildesF);
-		print_error("Error: Can't read from source file", 98);
-	}
-	return (Bread);
-}
-/**
- * write_to_destination - write content to file_to
- * @fildesT: fildescriptor of file_to
- * @bufr: char buffer
- * @Bread: read content to write
- */
-void write_to_destination(int fildesT, char *bufr, ssize_t Bread)
-{
-	ssize_t Bwriten = write(fildesT, bufr, Bread);
-
-	if (Bwriten == -1 || Bread != Bwriten)
-	{
 		close(fildesT);
-		print_error("Error: Can't write to destination file", 99);
+		print_error("Error: Can't read from source file", 98);
 	}
 }
 /**
@@ -98,27 +87,7 @@ void close_files(int fildesF, int fildesT)
 		print_error("Error: Can't close destination file descriptor", 100);
 	}
 }
-/**
- * cp - copies source file to dest file
- * @from: content file_from
- * @to: content file_to
- * Return: 0
- */
-int cp(const char *from, const char *to)
-{
-	int fildesF = open_source_file(from);
-	int fildesT = open_destination_file(to);
-	char bufr[1024];
-	ssize_t Bread;
 
-	while ((Bread = read_from_source(fildesF, bufr)) > 0)
-	{
-		write_to_destination(fildesT, bufr, Bread);
-	}
-
-	close_files(fildesF, fildesT);
-	return (0);
-}
 /**
  * main - takes arguments for cp
  * @argc: arg count
@@ -127,12 +96,19 @@ int cp(const char *from, const char *to)
  */
 int main(int argc, char *argv[])
 {
+	int fildesT, fildesF;
+
 	if (argc != 3)
 	{
 		print_error("Usage: cp file_from file_to", 97);
 	}
 
-	cp(argv[1], argv[2]);
+	fildesT = open_files(argv[1], argv[2]);
+	fildesF = open(argv[1], O_RDONLY);
+
+	copy_data(fildesF, fildesT);
+
+	close_files(fildesF, fildesT);
 
 	return (0);
 }
