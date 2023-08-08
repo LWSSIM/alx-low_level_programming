799b69341d9105883c98ffe2ecd6e128bec8cd2b
LWSSIM <wassim.elmnajja2@gmail.com>
2023-08-08 08:56:32 -0700

    modded edge cases

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -47,8 +47,10 @@ int open_files(const char *from, const char *to)
  * copy_data - copy data from filedescriptor to another
  * @fildesF: fildescriptor of file_from
  * @fildesT: fildescriptor of file_to
+ * @from: name of file_from
+ * @to: name of file_to
  */
-void copy_data(int fildesF, int fildesT)
+void copy_data(int fildesF, int fildesT, const char *from, const char *to)
 {
 	ssize_t Bread, Bwriten;
 	char bufr[1024];
@@ -59,16 +61,24 @@ void copy_data(int fildesF, int fildesT)
 
 		if (Bwriten == -1 || Bread != Bwriten)
 		{
+			char error_message[100];
+
+			snprintf(error_message, sizeof(error_message),
+					"Error: Can't write to %s", to);
 			close(fildesF);
 			close(fildesT);
-			print_error("Error: Can't write to destination file", 99);
+			print_error(error_message, 99);
 		}
 	}
 	if (Bread == -1)
 	{
+		char error_message[100];
+
+		snprintf(error_message, sizeof(error_message),
+				"Error: Can't read from file %s", from);
 		close(fildesF);
 		close(fildesT);
-		print_error("Error: Can't read from source file", 98);
+		print_error(error_message, 98);
 	}
 }
 /**
@@ -80,11 +90,19 @@ void close_files(int fildesF, int fildesT)
 {
 	if (close(fildesF) == -1)
 	{
-		print_error("Error: Can't close source file descriptor", 100);
+		char error_message[100];
+
+		snprintf(error_message, sizeof(error_message),
+				"Error: Can't close fd %i", fildesF);
+		print_error(error_message, 100);
 	}
 	if (close(fildesT) == -1)
 	{
-		print_error("Error: Can't close destination file descriptor", 100);
+		char error_message[100];
+
+		snprintf(error_message, sizeof(error_message),
+				"Error: Can't close fd %i", fildesT);
+		print_error(error_message, 100);
 	}
 }
 
@@ -106,7 +124,7 @@ int main(int argc, char *argv[])
 	fildesT = open_files(argv[1], argv[2]);
 	fildesF = open(argv[1], O_RDONLY);
 
-	copy_data(fildesF, fildesT);
+	copy_data(fildesF, fildesT, argv[1], argv[2]);
 
 	close_files(fildesF, fildesT);
 
