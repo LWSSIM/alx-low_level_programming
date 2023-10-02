a755951f2d7738fed451c618ce44aa70c7172349
LWSSIM <wassim.elmnajja2@gmail.com>
2023-10-03 00:57:53 +0100

    ELF modded

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,11 +2,15 @@
 #define MAIN_H
 
 #include <stddef.h>
-#include <unistd.h>
-#include <stdlib.h>
 #include <stdio.h>
 #include <fcntl.h>
+#include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
+#include <unistd.h>
+#include <elf.h>
+
+#define ERROR_MSG "Error: Not a valid ELF file.\n"
 
 /*reads a text file & print to stdout*/
 ssize_t read_textfile(const char *filename, size_t letters);
@@ -17,4 +21,9 @@ int create_file(const char *filename, char *text_content);
 /*append text to eof*/
 int append_text_to_file(const char *filename, char *text_content);
 
+/*elf data printer*/
+void print_elf_header_info(Elf64_Ehdr *header, int fd);
+
+/*elf executable fun*/
+int main(int argc, char *argv[]);
 #endif /*MAIN_H*/
