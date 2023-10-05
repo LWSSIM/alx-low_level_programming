f8117edffece48fd987a66d82c1f4c45086b0d72
LWSSIM <wassim.elmnajja2@gmail.com>
2023-10-05 16:48:07 +0100

    Format conformity plus betty for elfheader

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
- * print_OSABI - print type of ELF file
- * @header: pointer to ELF header array
+* print_OSABI - print type of ELF file
+* @header: pointer to ELF header array
 */
 void print_OSABI(Elf64_Ehdr *header)
 {
@@ -46,8 +46,8 @@ void print_OSABI(Elf64_Ehdr *header)
 }
 
 /**
- * print_Type - print type of ELF file
- * @header: pointer to ELF header array
+* print_Type - print type of ELF file
+* @header: pointer to ELF header array
 */
 void print_Type(Elf64_Ehdr *header)
 {
@@ -79,8 +79,8 @@ void print_Type(Elf64_Ehdr *header)
 }
 
 /**
- * print_Entry - prints the entry values of ELF header
- * @header: pointer to elf header array
+* print_Entry - prints the entry values of ELF header
+* @header: pointer to elf header array
 */
 void print_Entry(Elf64_Ehdr *header)
 {
@@ -89,30 +89,36 @@ void print_Entry(Elf64_Ehdr *header)
 	if (header->e_ident[EI_DATA] == ELFDATA2MSB)
 	{
 		header->e_entry = ((header->e_entry << 8) & 0xFF00FF00) |
-				((header->e_entry >>8) & 0xFF00FF);
-		header->e_entry = (header->e_entry << 16) | 
+				((header->e_entry >> 8) & 0xFF00FF);
+		header->e_entry = (header->e_entry << 16) |
 		(header->e_entry >> 16);
-	} 
+	}
 	if (header->e_ident[EI_CLASS] == ELFCLASS32)
 		printf("%#x\n", (unsigned int)header->e_entry);
+
 	else
 		printf("%#lx\n", header->e_entry);
 }
 /**
- * print_elf_header_info - printer
- * @header: file
- * @fd: file descriptor
+* print_elf_header_info - printer
+* @header: file
 */
 void print_elf_header_info(Elf64_Ehdr *header)
 {
 	int i;
+
 	char *small = "2's complement, little endian";
+
 	char *big = "2's complement, big endian";
 
 	printf("  Magic:   ");
 	for (i = 0; i < EI_NIDENT; i++)
 	{
-		printf("%02x ", header->e_ident[i]);
+		printf("%02x", header->e_ident[i]);
+		if (i == EI_NIDENT - 1)
+			printf("\n");
+		else
+			printf(" ");
 	}
 	printf("\n");
 	printf("  Class:                             ");
@@ -128,16 +134,17 @@ void print_elf_header_info(Elf64_Ehdr *header)
 	print_Entry(header);
 }
 /**
- * main - elf header printer program
- * @argc: ac
- * @argv: av
- *
- * Return: 0
+* main - elf header printer program
+* @argc: ac
+* @argv: av
+*
+* Return: 0
 */
 int main(int argc, char *argv[])
 {
 
 	int fd;
+
 	Elf64_Ehdr header;
 
 	if (argc != 2)
