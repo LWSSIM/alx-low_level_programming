1886667baaf09af9f2eaba01a46cce035a15e486
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-14 00:31:51 +0100

    updated keygen²

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -12,7 +12,7 @@
 int main(int ac, char **av)
 {
 	unsigned int i, j;
-	char *str = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
+	char *st = "A-CHRDw87lNS0E9B2TibgpnMVys5XzvtOGJcYLU+4mjW6fxqZeF3Qa1rPhdKIouk";
 	char arr[7] = "      ";
 	size_t lenght, increment;
 
@@ -22,31 +22,31 @@ int main(int ac, char **av)
 		return (1);
 	}
 	lenght = strlen(av[1]);
-	arr[0] = str[(lenght ^ 59) & 63];
+	arr[0] = st[(lenght ^ 59) & 63];
 
 	for (i = 0, increment = 0; i < lenght; i++)
 		increment += av[1][i];
 
-	arr[1] = str[(increment ^ 79) & 63];
+	arr[1] = st[(increment ^ 79) & 63];
 
 	for (i = 0, j = 1; i < lenght; i++)
 		j *= av[1][i];
-	arr[2] = str[(j ^ 85) & 63];
+	arr[2] = st[(j ^ 85) & 63];
 
 	for (j = av[1][0], i = 0; i < lenght; i++)
 		if ((char)j <= av[1][i])
 			j = av[1][i];
 	srand(j ^ 14);
 
-	arr[3] = str[rand() & 63];
+	arr[3] = st[rand() & 63];
 
 	for (j = 0, i = 0; i < lenght; i++)
 		j += av[1][i] * av[1][i];
-	arr[4] = str[(j ^ 239) & 63];
+	arr[4] = st[(j ^ 239) & 63];
 
 	for (j = 0, i = 0; (char)i < av[1][0]; i++)
 		j = rand();
-	arr[5] = str[(j ^ 229) & 63];
+	arr[5] = st[(j ^ 229) & 63];
 
 	printf("%s\n", arr);
 	return (0);
