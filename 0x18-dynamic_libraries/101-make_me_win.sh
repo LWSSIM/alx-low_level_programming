a67dfba22597bb770a0bade03356d69b80b44156
LWSSIM <wassim.elmnajja2@gmail.com>
2023-11-14 00:05:40 +0100

    update

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-wget -O /tmp/printf.so https://github.com/LWSSIM/alx-low_level_programming/blob/b0ebdd46fafcb4435c37950e5129f617d0d6abdf/0x18-dynamic_libraries/printf.so && export LD_PRELOAD=/tmp/printf.so
+curl -s -o ~/printf.so https://github.com/LWSSIM/alx-low_level_programming/blob/b0ebdd46fafcb4435c37950e5129f617d0d6abdf/0x18-dynamic_libraries/printf.so && export LD_PRELOAD=~/printf.so
