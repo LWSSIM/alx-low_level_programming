011f30aeb4334a5f8b27c8c7473d1339bdf5b449
LWSSIM <wassim.elmnajja2@gmail.com>
2024-03-12 16:08:31 +0000

    hidr

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -4,8 +4,9 @@
 #include <stddef.h>
 #include <stdio.h>
 
-/* Functions */
+/* Search Algorithms Functions */
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
 
 #endif /*SEARCH_ALGOS*/
